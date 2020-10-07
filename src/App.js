d27a93a50ad06dc4bcec40c23c668d7c9957c003
leeseungho02 <seungho020427@naver.com>
2020-10-07 09:25:05 +0900

    Update clone-coding-react-movie

--- src/App.js
+++ src/App.js
@@ -1,55 +1,17 @@
 import React from 'react';
-import axios from 'axios';
-import Movie from './Movie';
-import './App.css';
+import { Route, HashRouter } from "react-router-dom";
+import Home from './routes/Home';
+import About from './routes/About';
+import MovieDetail from './routes/MovieDetail';
+import Navigation from './components/Navigation';
 
-class App extends React.Component {
-	state = {
-		isLoading: true,
-		movies: []
-	};
-
-	getMovies = async () => {
-		const {
-			data: {
-				data: { movies }
-			}
-		} = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
-		this.setState({ movies, isLoading: false });
-	}
-
-	componentDidMount() {
-		this.getMovies();
-	}
-
-	render() {
-		const { isLoading, movies } = this.state;
-		return (
-			<div className="container">
-				{ isLoading ?
-					(
-						<div className="loader">
-							<span className="loader__text">loading...</span>
-						</div>
-					) : (
-						<div className="movies">
-							{movies.map(movie =>
-								<Movie
-									key={movie.id}
-									id={movie.id}
-									year={movie.year}
-									title={movie.title}
-									summary={movie.summary}
-									poster={movie.medium_cover_image}
-									genres={movie.genres}
-								/>
-							)}
-						</div>
-					)
-				}
-			</div>
-		);
-	}
+function App() {
+	return <HashRouter>
+		<Navigation />
+		<Route path="/" exact={true} component={Home}/>
+		<Route path="/about" component={About}/>
+		<Route path="/movie/:id" component={MovieDetail}/>
+	</HashRouter>;
 }
 
 export default App;
---
+++ src/routes/MovieDetail.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+class MovieDetail extends React.Component {
+    componentDidMount() {
+        const { location, history } = this.props;
+        if (location.state === undefined) {
+            history.push("/");
+        }
+    }
+
+    render() {
+        const { location } = this.props;
+        if(location.state){
+            return <div>{location.state.title}</div>;
+        } else {
+            return null;
+        }
+    }
+}
+
+export default MovieDetail;
